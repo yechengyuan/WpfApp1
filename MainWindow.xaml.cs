141a29e1143c60ae7a0203dbc0cef3b3f25c2a92
yechengyuan <YeChengyuan-P@outlook.com>
2020-05-30 00:10:52 +0800

    20200530

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace WpfApp1
 {
@@ -24,29 +25,55 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+         
+
+
         }
 
-     
-
-        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        private void btn01_Click(object sender, RoutedEventArgs e)
         {
-            var ofdTest=new OpenFileDialog();
-
-            if ((bool)ofdTest.ShowDialog().GetValueOrDefault())
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Title = "选择数据源文件";
+            openFileDialog.Filter = "txt文件|*.txt";
+            openFileDialog.FileName = string.Empty;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.DefaultExt = "txt";
+            if (openFileDialog.ShowDialog() == false)
             {
-                textFile1.Text = ofdTest.FileName;
-            }
-            else
-            {
-                textFile1.Text = null;
+                return;
             }
 
-         //   ofdTest.ShowDialog();
+            txtfile01.Text = openFileDialog.FileName;
+
+           
+         
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void btn02_Click(object sender, RoutedEventArgs e)
         {
 
+            var m_Dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = m_Dialog.ShowDialog();
+             
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            txtfile02.Text = m_Dialog.SelectedPath.Trim();
+        }
+
+        private void btn03_Click(object sender, RoutedEventArgs e)
+        {
+            var m_Dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = m_Dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            txtfile03.Text = m_Dialog.SelectedPath.Trim();
         }
     }
 }
